5d42da4f5c0a35afda6593abb3eb06b5178671d7
Moustafa Farhat <38836727+Farhat47@users.noreply.github.com>
2020-05-23 21:18:43 +0200

    9

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,22 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-
-
         int x = 10;
         int y = 20;
 
-        int z = add(x, y);
-    }
-
-    private String print(String input)
-    {
-        return input;
-    }
-    
-    private static int add(int x, int y) {
-        int z= x+y;
-        return z;
     }
 }
