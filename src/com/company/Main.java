2660fd5bb62ed4808374548eea573e2bf048917e
Moustafa Farhat <38836727+Farhat47@users.noreply.github.com>
2020-05-23 21:11:28 +0200

    6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+
         int x = 10;
         int y = 20;
 
         int z = add(x, y);
     }
 
+    public void printname(String name)
+    {
+        System.out.println(name);
+    }
     private static int add(int x, int y) {
         int z= x+y;
         return z;
