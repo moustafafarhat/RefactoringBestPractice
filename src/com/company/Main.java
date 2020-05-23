0b46265ef3fae725ef8e70736d0e2bbb8a10c022
Moustafa Farhat <38836727+Farhat47@users.noreply.github.com>
2020-05-23 21:01:15 +0200

    Extract Method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,11 @@ public class Main {
         int x = 10;
         int y = 20;
 
+        int z = add(x, y);
+    }
+
+    private static int add(int x, int y) {
         int z= x+y;
+        return z;
     }
 }
